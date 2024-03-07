9620e784c1336cc0b0707cc67bc764b711e21938
LimoDerEchte <mail@vogel-julian.de>
2024-03-07 22:57:30 +0100

    Implemented Settings Manager

--- app/src/main/java/com/limo/notecompanion/MainActivity.java
+++ app/src/main/java/com/limo/notecompanion/MainActivity.java
@@ -4,11 +4,11 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
+import com.limo.notecompanion.util.Settings;
 import maximsblog.blogspot.com.jlatexmath.cache.JLaTeXMathCache;
 import maximsblog.blogspot.com.jlatexmath.core.AjLatexMath;
 
 public class MainActivity extends AppCompatActivity {
-    public static boolean desktopMode = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,19 +20,19 @@ public class MainActivity extends AppCompatActivity {
 
         findViewById(R.id.latex_button).setOnClickListener((e) -> {
             Intent intent = new Intent(this, LatexEditor.class);
-            if(desktopMode)
+            if(Settings.desktopMode)
                 intent.setFlags(Intent.FLAG_ACTIVITY_LAUNCH_ADJACENT | Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_MULTIPLE_TASK);
             startActivity(intent);
         });
         findViewById(R.id.chemics_button).setOnClickListener((e) -> {
             Intent intent = new Intent(this, ReactionEditor.class);
-            if(desktopMode)
+            if(Settings.desktopMode)
                 intent.setFlags(Intent.FLAG_ACTIVITY_LAUNCH_ADJACENT | Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_MULTIPLE_TASK);
             startActivity(intent);
         });
         findViewById(R.id.settingsButton).setOnClickListener((e) -> {
             Intent intent = new Intent(this, SettingsActivity.class);
-            if(desktopMode)
+            if(Settings.desktopMode)
                 intent.setFlags(Intent.FLAG_ACTIVITY_LAUNCH_ADJACENT | Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_MULTIPLE_TASK);
             startActivity(intent);
         });
---
+++ app/src/main/java/com/limo/notecompanion/util/Settings.java
@@ -0,0 +1,48 @@
+package com.limo.notecompanion.util;
+
+import android.app.UiModeManager;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.res.Configuration;
+import androidx.appcompat.app.AppCompatDelegate;
+import com.limo.notecompanion.R;
+
+import java.util.Locale;
+
+public class Settings {
+    public static String language;
+    public static String theme;
+    public static String exportColor;
+    public static boolean desktopMode;
+
+    public static void load(Context ctx, AppCompatDelegate delegate) {
+        SharedPreferences pref = ctx.getSharedPreferences("default_settings", Context.MODE_PRIVATE);
+
+        language = pref.getString("language", "en_us");
+        theme = pref.getString("theme", "SYSTEM");
+        exportColor = pref.getString("exportColor", "INHERIT");
+        desktopMode = pref.getBoolean("desktopMode", isDesktopMode(ctx));
+
+        ctx.getResources().getConfiguration().setLocale(new Locale(language));
+        delegate.setLocalNightMode(theme.equalsIgnoreCase("SYSTEM") ?
+                AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM : theme.equalsIgnoreCase("LIGHT") ?
+                AppCompatDelegate.MODE_NIGHT_NO :
+                AppCompatDelegate.MODE_NIGHT_YES);
+        ctx.setTheme(theme.equalsIgnoreCase("DARK") ? R.style.Theme_NoteCompanion : R.style.Base_Theme_NoteCompanion);
+    }
+
+    public static void save(Context ctx) {
+        SharedPreferences pref = ctx.getSharedPreferences("default_settings", Context.MODE_PRIVATE);
+        SharedPreferences.Editor edit = pref.edit();
+        edit.putString("language", language);
+        edit.putString("theme", theme);
+        edit.putString("exportColor", exportColor);
+        edit.putBoolean("desktopMode", desktopMode);
+        edit.apply();
+    }
+
+    private static boolean isDesktopMode(Context context){
+        UiModeManager uiModeManager = (UiModeManager) context.getSystemService(Context.UI_MODE_SERVICE);
+        return (uiModeManager.getCurrentModeType() == Configuration.UI_MODE_TYPE_DESK);
+    }
+}
